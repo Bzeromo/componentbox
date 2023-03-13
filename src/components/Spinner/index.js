d7553c9e3bf334a3d5190e1978383204a8db000c
Bzeromo <dudrb5260@naver.com>
2023-03-13 14:08:46 +0900

    Toggle 추가

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -14,12 +14,12 @@ const Spinner = ({ size = 24, color='#919EAB', loading = true, ...props }) => {
         loading ? <Icon>
             <svg viewBox="0 0 38 38" xmlns="http:www.w3.org/2000/svg" style={sizeStyle}>
                 <g fill='none' fillRule='evenodd'>
-                    <g transfom='translate(1 1)'>
+                    <g transform='translate(1 1)'>
                         <path d='M36 18c0-9.94-8.06-18-18-18' stroke={color} strokeWidth='2'>
-                            <animateTransform attributeName='transform' type='rotate' from='0 18 18' to='360 18 18' dur='0.9s' repeateCount='indefinite'/>
+                            <animateTransform attributeName='transform' type='rotate' from='0 18 18' to='360 18 18' dur='0.9s' repeatCount='indefinite'/>
                         </path>
                         <circle fill={color} cx="36" cy="18" r="1">
-                            <animateTransform attributeName='transform' type='rotate' from='0 18 18' to='360 18 18' dur='0.9s' repeateCount='indefinite'/>
+                            <animateTransform attributeName='transform' type='rotate' from='0 18 18' to='360 18 18' dur='0.9s' repeatCount='indefinite'/>
                         </circle>
                     </g>
                 </g>
